ce60015aea66da1e7d95e35b0eb6341a72654da6
Gremlin80 <gremlin80@yopmail.com>
2017-08-13 21:56:37 +0200

    Add project files.

---
+++ Interventi/Interventi/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Interventi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = "Te lo credevi eh?";
+        }
+    }
+}
